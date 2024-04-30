e339ca1112760b9b65f0dbf933406dc989dd3098
ibrahimnassar24 <74038100+ibrahimnassar24@users.noreply.github.com>
2024-04-30 05:33:15 +0300

    remove PostFeed and SuggestedUsers from BasicLayout and add them to HomePage

--- src/layouts/BasicLayout.jsx
+++ src/layouts/BasicLayout.jsx
@@ -2,8 +2,7 @@ import {
     Flex, Box
 } from "@chakra-ui/react";
 import SideBar from "../components/SideBar/SideBar";
-import SuggestedUsers from "../components/SuggestedUsers/SuggestedUsers";
-import PostFeed from "../components/PostFeed/PostFeed";
+import HomePge from "../pages/HomePage";
 
 export default function BasicLayout() {
 
@@ -18,31 +17,11 @@ export default function BasicLayout() {
                     <SideBar />
                 </Box>
 
-                {/* middle section */}
-                <Flex
-                    direction="column"
-                    flex={1}
-                    bgColor="blackAlpha.200"
-                >
-                    <Box
-                        p={10}
-                    >
-
-                    </Box>
-                    
-                    <Box  h="full">
-                        <PostFeed />
-                    </Box>
-
-                </Flex>
-
-                <Box
-                    display={{base: "none", lg: "block"}}
-                    pe="15%"
-                    w="31vw"
-                >
-                    <SuggestedUsers />
+                {/* right section */}
+                <Box flex={1}>
+                    <HomePge />
                 </Box>
+
             </Flex>
         </>
     );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,40 @@
+import {
+    Flex, Box
+} from "@chakra-ui/react";
+import PostFeed from "../components/PostFeed/PostFeed";
+import SuggestedUsers from "../components/SuggestedUsers/SuggestedUsers";
 
 export default function HomePge() {
 
     return (
-        <>
-            <h1>Home</h1>
-        </>
+        <Flex h="100vh">
+            <Flex
+                direction="column"
+                flex={1}
+                bgColor="blackAlpha.200"
+            >
+                {/* stories section */}
+                <Box
+                    p={10}
+                >
+
+                </Box>
+
+                {/* post feed section */}
+                <Box h="full">
+                    <PostFeed />
+                </Box>
+
+            </Flex>
+
+            {/* suggested users section */}
+            <Box
+                display={{ base: "none", lg: "block" }}
+                pe="15%"
+                w="31vw"
+            >
+                <SuggestedUsers />
+            </Box>
+        </Flex>
     );
 };
